408712d95d938b9749713398c2eae01e1cc40910
gus-rkds <gusalv2001@gmail.com>
2023-06-18 23:35:50 -0400

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>Copyright © 2023 My Website</p>
+      </div>
+      <div>
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
